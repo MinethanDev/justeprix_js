3521a45c40b3bbdf03e2844c272cb519ed162d87
Minethan <ethanfjallet@gmail.com>
2024-02-03 14:14:20 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function genererNombreAleatoire() { // G
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+window.onload = function () {
+
+    var nombreAleatoire = genererNombreAleatoire();
+
+    window.nombreSecret = nombreAleatoire;
+
+    document.getElementById("essai_numero").innerText = "Premier essai";
+};
+
+var nombreEssais = 2;
+
+
+function envoyer_estimation() {
+    var estimationUtilisateur = parseInt(document.getElementById("estimation_joueur").value);
+
+    if (estimationUtilisateur === window.nombreSecret) {
+        document.getElementById("resultat").innerText = "Bravo ! Le nombre était bien " + window.nombreSecret;
+        document.getElementById("refresh_btn").style.display = "inline-block";
+    } else if (estimationUtilisateur < window.nombreSecret) {
+        document.getElementById("resultat").innerText = "C'est plus";
+    } else {
+        document.getElementById("resultat").innerText = "C'est moins";
+    }
+
+    document.getElementById("essai_numero").innerText = "Essai n°" + nombreEssais;
+
+    nombreEssais++;
+}
+
+function refresh() { // Bouton pour refresh la page
+    location.reload();
+}
